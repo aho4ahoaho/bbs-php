2441e376cc9e6bbf8c494bb9d18f08ed2af29fbf
aho4ahoaho <aho4ahoaho@outlook.jp>
2023-11-14 03:17:27 +0900

    mysql接続環境構築

--- src/index.php
+++ src/index.php
@@ -1,2 +1,30 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $servername = "db:3306";
+    $username = "user";
+    $password = "secret";
+    $dbname = "database";
+
+    // MySQLiオブジェクト指向
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // 接続チェック
+    if ($conn->connect_error) {
+        die("接続失敗: " . $conn->connect_error);
+    }
+
+    echo "接続成功";
+    ?>
+</body>
+
+</html>
